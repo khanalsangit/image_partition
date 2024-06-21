2eccf2dcf67c62736652a0441382e3ab0ce7ae0e
sangit khanal <khanalsangit2580@gmail.com>
2024-06-21 07:45:08 +0545

    change to line section

--- Data_fabrication.py
+++ Data_fabrication.py
@@ -6,19 +6,24 @@ from PIL import Image, ImageFont, ImageDraw
 
 
 def generate_random_string():
-    list=['*MRP NPR 200', '20 off - NPR 180/-', '4 Nos x 100g=400g', 'SAVE NPR 20 on 4Nos', '#16/06/24 B1']
+    # list=['*MRP NPR 200', '20 off - NPR 180/-', '4 Nos x 100g=400g', 'SAVE NPR 20 on 4Nos', '#16/06/24 B1']
+    line1=f'*MRP NPR {random.randrange(200,250)}'
+    line2=f'#{random.randrange(1,30)}/{random.randrange(1,12)}/24 B1'
+    line3=f'a'
+    list=[line1,line2,line3]
     characters = random.choice(list).split(" ")
     # print(characters)
     
-    rand_guess=random.choices([0,1],weights=(0.6,0.4))[0]
+    rand_guess=random.choices([0,1],weights=(0.6,0.4))[0]  #randomness control parameter with weights assigned
+    
     if rand_guess==0:
-        guess=random.choices([0,1],weights=(0.7,0.3))[0]
+        guess=random.choices([0,1],weights=(0.6,0.4))[0]
         if guess==0:
             sequence_dum=random.choice(characters)
-            sequence=random.choice(sequence_dum)
+            sequence=random.choice(sequence_dum)   #responsible for single character print
             
         else:
-            sequence=random.choice(characters)
+            sequence=random.choice(characters)     #responsible for single chunks of character print 
         
     else:
         choice1=random.choice(characters) 
@@ -30,9 +35,9 @@ def generate_random_string():
         else:
             rand_gues=random.choices([0,1],weights=(0.8,0.2))[0]
             if rand_gues==0:
-                sequence=choice1+' '+choice2
+                sequence=choice1+' '+choice2           #responsible for double chunks of character print 
             else:
-                sequence=choice1+' '+choice2+' '+choice3
+                sequence=choice1+' '+choice2+' '+choice3   #responsible for triple chunks of character print 
     return  sequence
 
 
@@ -44,13 +49,16 @@ def generate_random_string():
 #     # else:
 #     #     sequence=random.choice(characters)+' '+random.choice(characters)
 #     return  characters
-path="test.jpg"
-ouput_text="./ouput"
+
+path="test.jpg"    #blank image path
+ouput_text="./ouput"  #output folder for result
 for a in range(0,10):
     image = Image.open(path) 
 
     draw = ImageDraw.Draw(image) 
-    font = ImageFont.truetype(r"D:\computervision\PaddleOCR\fonts\fonts\inkjet\MerchantCopyDoublesize-jE7R.ttf", 30) 
+    font = ImageFont.truetype(r"D:\computervision\PaddleOCR\fonts\fonts\inkjet\MerchantCopyDoublesize-jE7R.ttf", 30)
+    #font=(text_font,text_size)
+
 # img = np.array(image)
 # omg=cv2.imread("pink_blank.jpg")
 # cv2.imshow("image",omg)
@@ -60,11 +68,10 @@ for a in range(0,10):
     for i in range(0,1):
         text = generate_random_string()
         # print(text)
-        
         # bbox = font.getbbox(text)    #getbbox() method returns a tuple (left, top, right, bottom)
         # text_width = bbox[2] - bbox[0]
         # text_height = bbox[3] - bbox[1]
-        draw.text((50, 19+i*60), text, fill=(0, 0, 0), font = font,spacing=20)   #(84, 55, 60)
+        draw.text((50, 19+i*60), text, fill=(0, 0, 0), font = font,spacing=20)   #place for display text
         # draw.rectangle((250-10,90+i*60,text_width+250,text_height+(10+90+i*60)),outline="yellow")
     # image.show()
 
